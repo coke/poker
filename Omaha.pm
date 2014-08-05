10addf32d15bf33581ede85468cea34562b96126
Will "Coke" Coleda <will@coleda.com>
2014-08-05 18:21:57 -0400

    move comment where the method's going

--- Omaha.pm
+++ Omaha.pm
@@ -29,23 +29,6 @@ class Deck {
     }
 }
 
-# Writeup Card Ranker - sort by high, sort by low.
-# Low - best 5 unique low cards rank only - if max > 8, no low
-# High - Named ranks:
-# Royal Flush / Straight Flush / 4 of a Kind / Full House /
-# Flush / Straight / 3 of a Kind / 2 Pair / Pair / High Card
-# Compare ranks first; 2 hands of same rank then compare by
-# cards in hand.
-#   properties - Any straight or flush - highest card wins
-#   full house - highest top card wins , then highest bottom card.
-#   3 of a kind - best Value card, then kicker1, then kicker2
-#   2 pair - best top card, then best bottom card, then best kicker
-#   1 pair - best pair card, best kicker1..3
-#   high card - best card, best kicker1..4
-
-# could do it by simple card ordering, but we need names like:
-# "set of threes with a king queen kicker"
-
 # in our output, show hand, then have rank of hands for high low.
 # anything ranked #1 in high/low show hand in bold. Anything else,
 # show numeric rank.
