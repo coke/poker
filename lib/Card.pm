eda07d8a0cda0baeeb4f18818d6285d06339f057
Will "Coke" Coleda <will@coleda.com>
2014-08-05 18:37:42 -0400

    moar refactor

--- lib/Card.pm
+++ lib/Card.pm
@@ -1,29 +1,7 @@
 use Rank;
-use Term::ANSIColor;
+use Suit;
 
 class Card {
-
-    enum Color <red black>;
-
-    class Suit {
-        has Color $.color;
-        has   Str $.name;
-        has   Str $.icon;
-        has   Int $.order;
-    
-        method Str {
-            return color("bold " ~ $.color) ~ color('bold') ~ $.icon ~ RESET;
-        }
-    }
-    
-    method Suits() {
-        return 
-            Suit.new(:order(1),:color(black),:name("Club"),:icon('♣')),
-            Suit.new(:order(2),:color(red),:name("Diamond"),:icon('♦')),
-            Suit.new(:order(3),:color(red),:name("Heart"),:icon('♥')),
-            Suit.new(:order(4),:color(black),:name("Spade"),:icon('♠'));
-    }
-    
     has Rank $.rank;
     has Suit $.suit;
     
--- lib/Deck.pm
+++ lib/Deck.pm
@@ -4,7 +4,7 @@ class Deck {
     has @.cards;
 
     submethod BUILD() {
-        for Card.Suits() X 1...13 -> $suit, $pips {
+        for Suit.all X 1...13 -> $suit, $pips {
             my $rank = Rank.new(:value($pips)); 
             my $card = Card.new(:$rank, :$suit);
             @!cards.push($card);
