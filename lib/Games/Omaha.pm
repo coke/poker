7706fe87b8308519ca5b683405b22f7e460f9205
Will "Coke" Coleda <will@coleda.com>
2014-12-01 21:45:39 -0500

    track spec changes

--- lib/Games/Omaha.pm
+++ lib/Games/Omaha.pm
@@ -5,17 +5,17 @@ class Games::Omaha {
     method lowScore($hand, $community) {
         # Best low score with two from your hand + the community.
         my $low = Inf; 
-        my $handRanks = $hand.list.map(-> $x {$x.rank.value}).grep(-> $x { $x <= 8 }).sort.uniq;
+        my $handRanks = $hand.list.map(-> $x {$x.rank.value}).grep(-> $x { $x <= 8 }).sort.unique;
         # need at least 2 lows in your hand for a low. 
         return 0 if $handRanks < 2; 
 
-        my $communityRanks = $community.list.map(-> $x {$x.rank.value}).grep(-> $x { $x <= 8 }).sort.uniq;
+        my $communityRanks = $community.list.map(-> $x {$x.rank.value}).grep(-> $x { $x <= 8 }).sort.unique;
         # need at least 3 lows in the community for a low.
         return 0 if $communityRanks < 3; 
 
         for iterate(2, $handRanks).lol -> $mycards {
             for iterate(3, $communityRanks).lol -> $tablecards {
-                my @ranks = ($mycards.list, $tablecards.list).flat.sort.uniq;
+                my @ranks = ($mycards.list, $tablecards.list).flat.sort.unique;
                 # we might have been counterfeited
                 next if +@ranks < 5; 
                 $low = min($low, [+] 1,10,100,1000,10000 Z* @ranks[0..^5]);
